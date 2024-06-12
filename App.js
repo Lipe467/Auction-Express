60b3097d0cda93866cb00a4c99f89846f7150a77
Lipe467 <132959145+Lipe467@users.noreply.github.com>
2024-06-12 19:33:26 -0300

    Add files via upload
    versão final do TCC da startup Auction Express

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
-import Routes from "./src/routes";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes';
+import Home from './src/screens/Home';
+import Login from './src/screens/Login';
+import Produto from './src/screens/Produto';
+
+
 
 export default function App() {
   return (
-      <Routes/>     
-
-  );
+    <Routes/>
+  )
 }
+
 
